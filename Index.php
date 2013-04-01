55ab9e4fbc67169decab84816f790c39951771d3
grupocova <marib.mcs@gmail.com>
2013-04-01 15:48:51 -0500

    Archivos Iniciales

---
+++ Index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css" />
+<link rel="stylesheet" type="text/css" href="css/base.css" />
+
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+  
+<title>Documento sin título</title>
+</head>
+
+<body>
+<div class="container">
+  <div class="row-fluid">
+    <!--<div class="span4">    
+    </div>-->
+    <div class="span12">
+    <div class="navbar">
+    <div class="navbar-inner">
+      <a class="brand" href="#">Title</a>
+      <ul class="nav">
+        <li><a href="#">EPG</a></li>
+        <li class="divider-vertical"></li>
+        <li><a href="#">Diplomados</a></li>
+        <li class="divider-vertical"></li>
+        <li><a href="#">Maestrias</a></li>
+        <li class="divider-vertical"></li>
+        <li><a href="#">Doctorados</a></li>
+        <li class="divider-vertical"></li>
+      </ul>
+   </div>
+   </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
