f50a9ee61ab30c45562e74055ebf7c9876e44625
grupocova <marib.mcs@gmail.com>
2013-04-02 11:31:23 -0500

    Creacion archivo Prueba y js

---
+++ Prueba.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css" />
+<link rel="stylesheet" type="text/css" href="css/base.css" />
+
+
+  
+<title> Sitio | Nombre</title>
+</head>
+
+<body>   
+<div class="navbar  navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">  
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          	<span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">Escuela de Posgrado</a>          
+            <ul class="nav" role="navigation">
+              <li class="dropdown">
+                  <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">EPG <b class="caret"></b> </a>
+                  <ul class="dropdown-menu" role="menu" aria-labelledby="dlabel">
+                  	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Historia</a></li>
+                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Misi&oacute;n y Visi&oacute;n </a></li>
+                  </ul>
+              </li>
+              <li class="divider-vertical"></li>
+              <li><a href="#">Diplomados</a></li>
+              <li class="divider-vertical"></li>
+              <li><a href="#">Maestrias</a></li>
+              <li class="divider-vertical"></li>
+              <li><a href="#">Doctorados</a></li>
+              <li class="divider-vertical"></li>
+            </ul>   
+            <form class="navbar-search pull-right">
+            <input type="text" class="search-query" placeholder="search" /> 
+            </form>         
+          </div>          
+        </div>     
+      </div>
+   	</div>
+    
+    <div class="container">
+    	<div class="hero-unit">
+        <h1>GRUPO COVA</h1>
+        <p>Sitio desarrollado para ...</p>
+        <p> <a href="#" class="btn btn-primary btn-large" target="_blank">Ver m&aacute;s</a> </p>
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap-transition.js"></script>
+  <script src="js/bootstrap-alert.js"></script>
+  <script src="js/bootstrap-modal.js"></script>
+  <script src="js/bootstrap-dropdown.js"></script>
+  <script src="js/bootstrap-scrollspy.js"></script>
+  <script src="js/bootstrap-tab.js"></script>
+  <script src="js/bootstrap-tooltip.js"></script>
+  <script src="js/bootstrap-popover.js"></script>
+  <script src="js/bootstrap-button.js"></script>
+  <script src="js/bootstrap-collapse.js"></script>
+  <script src="js/bootstrap-carousel.js"></script>
+  <script src="js/bootstrap-typeahead.js"></script>
+  <script src="js/bootstrap-affix.js"></script>
+  <script type="text/javascript">
+  $('.dropdown-togle').dropdown()
+  </script>
+
+    
+</body>
+</html>
