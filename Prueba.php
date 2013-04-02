56cb061d453cdf9ae868e7864b56f6edd8ac81ca
grupocova <marib.mcs@gmail.com>
2013-04-02 17:44:07 -0500

    avance main y footer

--- Prueba.php
+++ Prueba.php
@@ -6,7 +6,8 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css" />
 <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css" />
-<link rel="stylesheet" type="text/css" href="css/base.css" />
+<link rel="stylesheet" type="text/css" href="css/EBase.css" />
+<link rel="stylesheet" type="text/css" href="css/prettify.css" />
 
 
   
@@ -48,10 +49,113 @@
    	</div>
     
     <div class="container">
-    	<div class="hero-unit">
+      <div class="hero-unit">
         <h1>GRUPO COVA</h1>
         <p>Sitio desarrollado para ...</p>
         <p> <a href="#" class="btn btn-primary btn-large" target="_blank">Ver m&aacute;s</a> </p>
+      </div>
+      <div class="row show-grid">
+      	<div class="span3 panel">
+        <div class="bs-docs-example">
+        	<h3 class="well well-small">Agenda</h3>
+        </div>
+            <p>Button groups can also function as radios, where only one button may be active, or checkboxes, where any number of buttons may be active. View the JavaScript docs for that.</p>
+        </div>
+        
+        <div class="span6 panel">
+        	<div class="bs-docs-example">
+              <h3 class="well well-small">Noticias</h3>
+            </div>
+            <div class="media">
+              <a class="pull-left" href="#">
+                <img class="media-object" data-src="holder.js/120x70">
+              </a>
+              <div class="media-body">
+                <h4 class="media-heading">Media heading</h4>
+                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+              </div>
+            </div>
+            
+            <div class="media">
+              <a class="pull-left" href="#">
+                <img class="media-object" data-src="holder.js/120x70">
+              </a>
+              <div class="media-body">
+                <h4 class="media-heading">Media heading</h4>
+               <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+              </div>
+            </div>
+            
+            <div class="media">
+              <a class="pull-left" href="#">
+                <img class="media-object" data-src="holder.js/120x70">
+              </a>
+              <div class="media-body">
+                <h4 class="media-heading">Media heading</h4>
+               <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+              </div>
+            </div>
+        </div>
+        
+        <div class="span3 panel">
+        	<div class="bs-docs-example">
+        	<h3 class="well well-small">Siguenos</h3>
+            </div>
+            <p>Button groups can also function as radios, where only one button may be active, or checkboxes, where any number of buttons may be active. View the JavaScript docs for that.</p>
+        </div>
+    </div>   
+    
+<footer class="footer">
+      <div class="container">       
+        <ul class="footer-links row">
+        <div class="span3">
+          <li><a href="#">EPG</a>
+          	<ul>
+              <li><a href="#">Presentacion</a></li>
+              <li><a href="#">Mision y Vision</a></li>
+              <li><a href="#">Autoridades</a></li>
+            </ul>
+          </li> 
+        </div> 
+        <div class="span3">    
+          <li><a href="#">DIPLOMADOS</a>
+            <ul>
+              <li><a href="#">Presentacion</a></li>
+              <li><a href="#">Mision y Vision</a></li>
+              <li><a href="#">Autoridades</a></li>
+            </ul>
+          </li>  
+          <li><a href="#">MAESTRIAS</a>
+            <ul>
+              <li><a href="#">Presentacion</a></li>
+              <li><a href="#">Mision y Vision</a></li>
+              <li><a href="#">Autoridades</a></li>
+            </ul>
+          </li>  
+        </div>    
+        <div class="span3">
+          <li><a href="#">DOCTORADOS</a>
+            <ul>
+              <li><a href="#">Presentacion</a></li>
+              <li><a href="#">Mision y Vision</a></li>
+              <li><a href="#">Autoridades</a></li>
+            </ul>
+          </li>  
+        </div>
+        <div class="span3">
+          <li><a href="#">INFORMES</a>
+            <ul>
+              <li><a href="#">Presentacion</a></li>
+              <li><a href="#">Mision y Vision</a></li>
+              <li><a href="#">Autoridades</a></li>
+            </ul>
+          </li>  
+          <li><a href="#">CAMPUS VIRTUAL</a></li>
+        </div>  
+        </ul>
+      </div>
+    </footer> 
+     
   <script src="js/jquery.js"></script>
   <script src="js/bootstrap-transition.js"></script>
   <script src="js/bootstrap-alert.js"></script>
@@ -66,6 +170,7 @@
   <script src="js/bootstrap-carousel.js"></script>
   <script src="js/bootstrap-typeahead.js"></script>
   <script src="js/bootstrap-affix.js"></script>
+  <script src="js/holder.js"></script>
   <script type="text/javascript">
   $('.dropdown-togle').dropdown()
   </script>
