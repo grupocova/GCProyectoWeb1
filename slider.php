900db83c466ae8f331df825f9f8e088e71a72a11
Bryan Valdez <invictvs22@gmail.com>
2013-04-03 11:50:47 -0500

    agrego base slider y cambios

---
+++ slider.php
@@ -0,0 +1,5 @@
+<div class="hero-unit">
+<h1>Banner</h1>
+<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+<p><a href="#" class="btn btn-primary btn-large">Banner1</a></p>
+</div>
