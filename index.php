4e59fec1c0bbfe24a96ab7d8ad6d6d7cc29cb9ec
unknown <maribel.condori@FIA-WEBMASTER.lim.upeu.edu.pe>
2013-08-15 17:29:54 -0500

    eliminacion de demo de slider

--- index.php
+++ index.php
@@ -13,9 +13,7 @@
 <link rel="stylesheet" type="text/css" href="amazing/css/initcarousel-1.css">
 <link rel="stylesheet" type="text/css" href="amazing/css/initcarousel-2.css">
 
-<!-- css evoslider -->
-<link rel="Stylesheet" type="text/css" href="evoslider/css/evoslider.css" />
-<link rel="Stylesheet" type="text/css" href="evoslider/css/default.css" /> 
+<!-- css evoslider --> 
 <!-- css web -->
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 
@@ -57,34 +55,7 @@
 <script src="amazing/js/initcarousel-1.js"></script>
 <script src="amazing/js/initcarousel-2.js"></script>
 <!-- js evoslider -->
-<script type="text/javascript" src="evoslider/js/jquery.evoslider.lite-1.1.0.js"></script>
 
-<script type="text/javascript">
-            
-    $("#mySlider").evoSlider({
-        mode: "slider",                  // Sets slider mode ("accordion", "slider", or "scroller")
-        // width: 400,                         // The width of slider
-        // height: 100,                        // The height of slider
-        slideSpace: 5,                      // The space between slides
-    
-        mouse: true,                        // Enables mousewheel scroll navigation
-        keyboard: true,                     // Enables keyboard navigation (left and right arrows)
-        speed: 500,                         // Slide transition speed in ms. (1s = 1000ms)
-        easing: "swing",                    // Defines the easing effect mode
-        loop: true,                         // Rotate slideshow
-    
-        autoplay: true,                     // Sets EvoSlider to play slideshow when initialized
-        interval: 5000,                     // Slideshow interval time in ms
-        pauseOnHover: true,                 // Pause slideshow if mouse over the slide
-        pauseOnClick: true,                 // Stop slideshow if playing
-        
-        directionNav: true,                 // Shows directional navigation when initialized
-        directionNavAutoHide: false,        // Shows directional navigation on hover and hide it when mouseout
-    
-        controlNav: true,                   // Enables control navigation
-        controlNavAutoHide: false           // Shows control navigation on mouseover and hide it when mouseout 
-    });                                
-    
-</script>
+
 </body>
 </html>
