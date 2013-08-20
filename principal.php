7e56d6b1bf9242186944b5c3a08769c59e1c9665
unknown <maribel.condori@FIA-WEBMASTER.lim.upeu.edu.pe>
2013-08-20 16:03:37 -0500

    propuesta posicionamiento contenidos home

--- principal.php
+++ principal.php
@@ -1,11 +1,97 @@
 <!---->
 <div class="row">
-  <!--calendario - eventos-->
-  <div class="col-lg-3 wepg-panel" id="wepg-eventos">
-    <div class="title">Agenda</div>
-    <div id="amazingcarousel-1">
-      <div class="amazingcarousel-list-container" style="">
+  <!--Panel extra -->
+  <div class="col-lg-12 panel" id"wepg-extra">
+    <div class="" id="wepg-av">
+      <!-- <img src="fotos/images.jpg"> -->
+      <ul class="">
+      <li>
+        <span class="">Small</span>
+        <span class="">&#10004; Visible on small</span>
+      </li>
+      <li>
+        <span class="">Medium</span>
+        <span class="">&#10004; Visible on medium</span>
+      </li>
+      <li>
+        <span class="">Large</span>
+        <span class="g">&#10004; Visible on large</span>
+      </li>
+    </ul>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <!--panel izquierdo-->
+  <div class="col-lg-8 panel">
+
+    <!--Anuncios secundarios-->
+    <div class="wepg-panel">
+      <div class="title">Anuncios secundarios</div>
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="media-object" data-src="holder.js/450x200">
+        </a>
+        <div class="media-body">
+          <h4 class="media-heading">Media heading</h4>
+          ...
+          </div>
+      </div>
+    </div> 
+
+    <!--Noticias--> 
+    <div class="wepg-panel" id="wepg-noticia">
+      <div class="title">Noticias</div>
+      <div id="amazingcarousel-2">
+      <div class="amazingcarousel-list-container">
         <ul class="amazingcarousel-list">
+          <li class="amazingcarousel-item">
+            <div class="amazingcarousel-item-container">
+              <div class="media panel">
+                <a class="pull-left" href="#">
+                  <img class="media-object" src="fotos/noticia1.jpg">
+                </a>
+                <div class="media-body">
+                  <h4 class="media-heading">Sustentación de Tesis - UPG Educación</h4>
+                  <p>El pasado jueves 14 de Febrero de 2013, la Lic. Marina del Águila Gonzáles sustentó su trabaj...</p>
+                </div>
+              </div>
+              <div style="clear:both;"></div>
+            </div>            
+          </li>
+          <li class="amazingcarousel-item">
+            <div class="amazingcarousel-item-container">
+              <div class="media panel">
+                <a class="pull-left" href="#">
+                  <img class="media-object" src="fotos/noticia2.png">
+                </a>
+                <div class="media-body">
+                  <h4 class="media-heading">Escuela de Posgrado - Convocatoria MBA 2013</h4>
+                  <p>La Unidad de Posgrado en Administración, dio inicio a su convocatoria a la Maestría en ...</p>
+                </div>
+              </div>
+              <div style="clear:both;"></div>
+            </div>            
+          </li>
+        </ul>
+      </div>
+      <div class="amazingcarousel-prev"></div>
+      <div class="amazingcarousel-next"></div>
+      <div class="amazingcarousel-engine" style="display:none;"><a href="http://amazingcarousel.com">JavaScript Image Scroller</a></div>
+    </div>
+    </div>    
+  </div>
+
+  <!--panel derecho-->
+  <div class="col-lg-4 panel">
+
+    <!--Eventos- calendario-->  
+    <div class="wepg-panel" id="wepg-eventos">
+      <div class="title">Agenda</div>
+      <div id="amazingcarousel-1">
+        <div class="amazingcarousel-list-container" style="">
+          <ul class="amazingcarousel-list">
 
           <li class="amazingcarousel-item">
             <div class="amazingcarousel-item-container">
@@ -60,101 +146,18 @@
             </div>            
             <div style="clear:both;"></div>    
           </li>          
-        </ul>
-      </div>
-      <div class="amazingcarousel-prev"></div>
-      <div class="amazingcarousel-next"></div>
-    </div>
-<!-- End of body section HTML codes -->
-  </div>
-
-  <!--Anuncios Secundarios-->
-  <div class="col-lg-6 wepg-panel" id="wepg-anunsec">
-    <div class="title">Anuncios secundarios</div>
-      
-           
-  </div>
-
-  <!--extra-->
-  <div class="col-lg-3">
-    <div class="row">
-      <div class="col-lg-12 wepg-panel" id="wepg-av">
-        <div class="title">Aula Virtual</div>
-        <img src="fotos/images.jpg" style"">
+          </ul>
+        </div>
+        <div class="amazingcarousel-prev"></div>
+        <div class="amazingcarousel-next"></div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-lg-12 wepg-panel" id="wepg-">
-        <ul id="myTab" class="nav nav-tabs md-tab-nav">
-          <li class="active"><a href="#tag1" data-toggle="tab">Social</a></li>
-          <li><a href="#tag2" data-toggle="tab">Multimedia</a></li>      
-      </ul>
 
-      <div id="myTabContent" class="tab-content md-tab-cont">
-        <!---->
-        <div class="tab-pane fade in active" id="tag1">   
-          <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.Cras purus odio, vestibulum in.
-          </p>          
-        </div><!--Fin -->
-
-        <!---->
-        <div class="tab-pane fade" id="tag2">
-          <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in. 
-         </p>          
-        </div><!--Fin -->
-      </div>
-      </div>
-    </div>
-  </div><!--Fin extra-->
-
-</div><!--Fin row-->
-
-<div class="row">
-  <div class="col-lg-9 wepg-panel" id="wepg-noticia">
-    <div class="title">Noticias</div>
-    <div id="amazingcarousel-2">
-      <div class="amazingcarousel-list-container">
-        <ul class="amazingcarousel-list">
-          <li class="amazingcarousel-item">
-            <div class="amazingcarousel-item-container">
-              <div class="media panel">
-                <a class="pull-left" href="#">
-                  <img class="media-object" src="fotos/noticia1.jpg">
-                </a>
-                <div class="media-body">
-                  <h4 class="media-heading">Sustentación de Tesis - UPG Educación</h4>
-                  <p>El pasado jueves 14 de Febrero de 2013, la Lic. Marina del Águila Gonzáles sustentó su trabaj...</p>
-                </div>
-              </div>
-              <div style="clear:both;"></div>
-            </div>            
-          </li>
-          <li class="amazingcarousel-item">
-            <div class="amazingcarousel-item-container">
-              <div class="media panel">
-                <a class="pull-left" href="#">
-                  <img class="media-object" src="fotos/noticia2.png">
-                </a>
-                <div class="media-body">
-                  <h4 class="media-heading">Escuela de Posgrado - Convocatoria MBA 2013</h4>
-                  <p>La Unidad de Posgrado en Administración, dio inicio a su convocatoria a la Maestría en ...</p>
-                </div>
-              </div>
-              <div style="clear:both;"></div>
-            </div>            
-          </li>
-        </ul>
-      </div>
-      <div class="amazingcarousel-prev"></div>
-      <div class="amazingcarousel-next"></div>
-      <div class="amazingcarousel-engine" style="display:none;"><a href="http://amazingcarousel.com">JavaScript Image Scroller</a></div>
-    </div>
-  </div>
-
-  <div class="col-lg-3 wepg-panel" id="wepg-publi">
-    <div class="title">Publicaciones</div>
-    <div id="amazingcarousel-3">
-          <div class="amazingcarousel-list-container">
+    <!--Eventos- calendario-->  
+    <div class="wepg-panel" id="wepg-publi">
+      <div class="title">Publicaciones</div>
+      <div id="amazingcarousel-3">
+        <div class="amazingcarousel-list-container">
             <ul class="amazingcarousel-list">
               <li class="amazingcarousel-item">
                 <div class="amazingcarousel-item-container">
@@ -171,5 +174,11 @@
           <div class="amazingcarousel-prev"></div>
           <div class="amazingcarousel-next"></div>
         </div>
+    </div>
+
+
+
   </div>
+
+  
 </div><!--Fin row-->
